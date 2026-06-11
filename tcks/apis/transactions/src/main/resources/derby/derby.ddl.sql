1ac7e5e05a2aa0c3530d9b0b082b7d3292dbbb19
aidentzhang <aidentzhang@gmail.com>
2026-06-11 10:52:47 -0400

    Migrated Certificates and Derby tables into TCK

---
+++ tcks/apis/transactions/src/main/resources/derby/derby.ddl.sql
@@ -0,0 +1,40 @@
+-- JTA Transaction Propagation Test Tables
+drop table JTA_Tab1 ;
+create table JTA_Tab1 (KEY_ID int, COF_NAME varchar(32), PRICE float) ;
+
+drop table JTA_Tab2 ;
+create table JTA_Tab2 (KEY_ID int, CHOC_NAME varchar(32), PRICE float) ;
+
+-- Security/Authentication Tables (required by TCK infrastructure)
+DROP TABLE caller ;
+DROP TABLE caller_groups ;
+
+CREATE TABLE caller(name VARCHAR(64) PRIMARY KEY, password VARCHAR(1024)) ;
+CREATE TABLE caller_groups(caller_name VARCHAR(64), group_name VARCHAR(64)) ;
+
+INSERT INTO caller VALUES('tom', 'secret1') ;
+INSERT INTO caller VALUES('emma', 'secret2') ;
+INSERT INTO caller VALUES('bob', 'secret3') ;
+
+INSERT INTO caller_groups VALUES('tom', 'Administrator') ;
+INSERT INTO caller_groups VALUES('tom', 'Manager') ;
+
+INSERT INTO caller_groups VALUES('emma', 'Administrator') ;
+INSERT INTO caller_groups VALUES('emma', 'Employee') ;
+
+INSERT INTO caller_groups VALUES('bob', 'Administrator') ;
+
+ INSERT INTO caller VALUES('tom_hash512_saltsize16', 'PBKDF2WithHmacSHA512:1024:DbjXqT9p8VhJ7OtU6DrqDw==:p/qihG8IZKkz03JzKd6XXA==') ;
+ INSERT INTO caller VALUES('tom_hash256_saltsize32', 'PBKDF2WithHmacSHA256:2048:suVayUIJMQMc6wCgckvAIgKRlo1UkxyFXhXbTxX6C7s=:cvdHkBXVUCN2WL3LRAYodeCdNZxEM4RLlNCCYP68Kmg=') ;
+ INSERT INTO caller VALUES('tom_hash512_saltsize32', 'PBKDF2WithHmacSHA512:2048:dPTjUfiklfyg2bas/KOQKqEfdtoXK8YvbBscIxA8tNg=:ixBg0wr3ySBI86y8HP7+Yw==') ;
+
+ INSERT INTO caller_groups VALUES('tom_hash512_saltsize16', 'Administrator') ;
+ INSERT INTO caller_groups VALUES('tom_hash512_saltsize16', 'Manager') ;
+
+ INSERT INTO caller_groups VALUES('tom_hash256_saltsize32', 'Administrator') ;
+ INSERT INTO caller_groups VALUES('tom_hash256_saltsize32', 'Manager') ;
+
+ INSERT INTO caller_groups VALUES('tom_hash512_saltsize32', 'Administrator') ;
+ INSERT INTO caller_groups VALUES('tom_hash512_saltsize32', 'Manager') ;
+
+-- Made with Bob
